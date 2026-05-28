b1599b9b832a1ecbfd0c80bcbc84c416277c6e1f
Dharo Gabriel Fernandez <gabriel.fernandez8786@gmail.com>
2026-05-28 16:38:00 -0500

    Update BaseTest.java

--- src/test/java/utils/BaseTest.java
+++ src/test/java/utils/BaseTest.java
@@ -1,63 +1,21 @@
-package utils;
-
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.By;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-
-import java.time.Duration;
-
-public class BaseTest {
-    protected WebDriver driver;
-    protected WebDriverWait wait;
-    protected static ExtentReports extent;
-    protected static ExtentTest test;
-
-    @BeforeSuite
-    public void setupReport() {
-        extent = ExtentManager.getInstance();
-    }
-
-    @AfterSuite
-    public void flushReport() {
-        if (extent != null) {
-            extent.flush();
-        }
-    }
-
-    @BeforeMethod
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--window-size=1920,1080");
-        
-        driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        
-        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        
-        driver.get("https://opensource-demo.orangehrmlive.com/");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+@BeforeMethod
+public void setUp() {
+    WebDriverManager.chromedriver().setup();
+    
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless");
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--disable-gpu");
+    options.addArguments("--window-size=1920,1080");
+    options.addArguments("--remote-allow-origins=*");
+    
+    driver = new ChromeDriver(options);
+    // No usar maximize() en headless, ya tenemos window-size
+    // driver.manage().window().maximize();
+    
+    wait = new WebDriverWait(driver, Duration.ofSeconds(15)); // Aumenté a 15
+    
+    driver.get("https://opensource-demo.orangehrmlive.com/");
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
 }
